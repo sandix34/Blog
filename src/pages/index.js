e5f98e0993b2dae64bfb1d3205fa2ae4293e4d99
sandrine <sandrinezocca@gmail.com>
2019-06-05 17:01:06 +0200

    requête graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import GLayout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <GLayout page="1">
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>{data.site.siteMetadata.title}</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -17,5 +17,15 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
   </GLayout>
 )
+
+export const query = graphql`
+query {
+  site {
+    siteMetadata {
+      title
+    }
+  }
+}
+`
 
 export default IndexPage
